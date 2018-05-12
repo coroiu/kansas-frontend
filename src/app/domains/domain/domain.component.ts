e698275fc4e7260ea76f8c3ee6e85ad8f7e66c43
Andreas Coroiu <andreas.coroiu@gmail.com>
2018-05-12 16:44:19 +0200

    Make some design changes.

--- src/app/domains/domain/domain.component.ts
+++ src/app/domains/domain/domain.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { DomainModel } from '../domains.model';
 
 @Component({
   selector: 'app-domain',
   templateUrl: './domain.component.html',
-  styleUrls: ['./domain.component.scss']
+  styleUrls: ['./domain.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DomainComponent implements OnInit {
   @Input() domain: DomainModel;
